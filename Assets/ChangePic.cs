239613ec08205981bebafded3e48a6adafdbf9d1
Luke Olson <lolson31@huskers.unl.edu>
2022-01-22 22:46:45 -0600

    Slideshow Feature
    Added images for slide show,
    Added ability for screen to load and display images,
    Added ability to change images via controller

---
+++ Assets/ChangePic.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePic : MonoBehaviour
+{
+    //Set these Textures in the Inspector
+    Renderer m_Renderer;
+    private Object[] textures;
+    private int currSlide = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the Renderer from the GameObject
+        m_Renderer = GetComponent<Renderer>();
+
+        textures = Resources.LoadAll("Textures", typeof(Texture2D));
+
+        //Set the Texture you assign in the Inspector as the main texture (Or Albedo)
+        m_Renderer.material.SetTexture("_MainTex", (Texture2D)textures[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            currSlide++;
+            if (currSlide == textures.Length)
+            {
+                currSlide = 0;
+            }
+            m_Renderer.material.SetTexture("_MainTex", (Texture2D)textures[currSlide]);
+        }
+    }
+}
